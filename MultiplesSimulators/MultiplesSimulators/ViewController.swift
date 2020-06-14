d852e01342c8df04e322ddaac23249e3faceae0f
Jefferson Lalor <jeffersonlalor@ioasys.com.br>
2020-06-14 14:56:06 -0400

    Created xcode project

---
+++ MultiplesSimulators/MultiplesSimulators/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultiplesSimulators
+//
+//  Created by Jefferson Lalor on 14/06/20.
+//  Copyright © 2020 Jefferson Lalor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
